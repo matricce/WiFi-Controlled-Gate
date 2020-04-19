c94ab5d0937e4bc1587ec5107e170a10ba4a5403
matricce <ruviansanches20@gmail.com>
2020-04-19 20:04:00 -0300

    Update index.h

--- index.h
+++ index.h
@@ -6,37 +6,17 @@ const char webSiteContent[] PROGMEM = R"=====(
 <meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1.0, maximum-scale=1.0"/>
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
 <script>
   var websock;
   var t0 = 0;
   var t1 = 0;
+  var state = 0;
+    
   const socketMessageListener = (event) => {
-    console.log(event.data);
-  };
-
-  const socketOpenListener = (event) => {
-    console.log('Connected');
-  };
-
-  const socketCloseListener = (event) => {
-    if (websock) {
-      console.error('Disconnected.');
-    }
-    websock = new WebSocket("ws://" + window.location.hostname + ":88/");
-    websock.addEventListener('open', socketOpenListener);
-    websock.addEventListener('message', socketMessageListener);
-    websock.addEventListener('close', socketCloseListener);
-  };
-  socketCloseListener();
-  InitWebSocket();
-  function InitWebSocket() {
-    websock.onmessage = function(evt) {
-      JSONobj = JSON.parse(evt.data);
-      document.getElementById('btn').innerHTML = JSONobj.controlOn;
-      document.getElementById('timeOn').innerHTML = 'Ligado há: ' + JSONobj.timeOn;
-      t0 = performance.now();
-
+    t0 = performance.now();
+          JSONobj = JSON.parse(event.data);
+      state = JSONobj.controlOn;
+      document.getElementById('timeOn').innerHTML = 'Ligado há: ' + JSONobj.timeOn; 
       if(JSONobj.controlOn == 'ON') {
         document.getElementById('btn').style.background = '#E3E3E3';
         document.getElementById('btn').style.color = '#434343';
@@ -47,19 +27,35 @@ const char webSiteContent[] PROGMEM = R"=====(
         document.getElementById('btn').style.color = '#E3E3E3';
         document.getElementById('btn').style["boxShadow"]="0px 0px 0px 8px #434343";
       }
+  };
+
+  const socketOpenListener = (event) => {
+    console.log('Connected');
+  };
+
+  const socketCloseListener = (event) => {;
+    if (websock) {
+      console.error('Disconnected.');
     }
-  }
+    websock = new WebSocket("ws://" + window.location.hostname + ":88/");
+    websock.addEventListener('open', socketOpenListener);
+    websock.addEventListener('message', socketMessageListener);
+    websock.addEventListener('close', socketCloseListener);
+  };
+
+  socketCloseListener();
+  
   function button() {
-    if(document.getElementById('btn').innerHTML === 'OFF') {
+    if(state === 'OFF') {
       websock.send('LEDonoff=ON');
     }
   }
   function forceReload() {
-    setInterval(verify, 1000);
+    setInterval(verify, 2000);
   }
   function verify() {
     if(t1 - t0 > 5000) {
-      InitWebSocket();
+      location.reload(0);
     }
     t1 = performance.now();
   }
@@ -87,11 +83,15 @@ h5{
   font-weight: bold;
   color: #B2DFDB ;
 }
-i{
-  position: absolute;
-  left: 85%;
-  right: 5%;
-  top: 20%;
+#refresh{
+  display: flex;
+  margin-left:85%;
+  margin-right:0px;
+  margin-top: -60px;
+  width:43px;
+}
+.fa-refresh{
+  display: inline-block;
   color: #B2DFDB;
   }
 #mid{
@@ -102,19 +102,23 @@ i{
   background: #009688;
 }
 #btn{
-  display: inline-block;
+  display: table;
   text-decoration:none;
   background: #434343;
   color: #434343;
-  font: 100px arial, sans-serif;
   line-height: 320px;
   width: 320px;
   height: 320px;
   border-radius: 20%;
-  text-align: center;
   box-shadow: 0px 0px 0px 8px #434343;
   border: solid 2px rgba(255,255,255,0.47);
-  transition: 0.4s;  
+  transition: 0.3s;
+}
+.fa-bolt{
+  font-size:220px;
+  display: table-cell;
+  vertical-align: middle;
+  text-align: center;
 }
 #footer{
   position: absolute;
@@ -130,10 +134,12 @@ i{
 <body ONLOAD="forceReload()">
   <div id="header">
     <h5>Controle Portão</h5>
-    <a href="#" ONCLICK='InitWebSocket()'><i href="." class="fa fa-refresh"></i></a>
+    <div id="refresh">
+      <a href="#" ONCLICK='location.reload(0)'><i href="." class="fa fa-refresh"></i></a>
     </div>
+  </div>
 <div id="mid">
-  <a href="#" id="btn" ONCLICK='button()'></a>
+  <a href="#" id="btn" ONCLICK='button()'><i class="fa fa-bolt"></i></a>
 </div>
 <div id="footer">
   <h2 id="timeOn"></h2>

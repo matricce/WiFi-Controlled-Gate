006182044684dd45d13c691a5556dc7377b14ba6
matricce <ruviansanches20@gmail.com>
2020-04-21 16:21:36 -0300

    Update index.h
    mudanças pra melhorar o codestyle

--- index.h
+++ index.h
@@ -1,65 +1,12 @@
 const char webSiteContent[] PROGMEM = R"=====(
 <html>
+<title>Controle Portão</title>
 <link rel="icon" href="https://t4.ftcdn.net/jpg/01/85/91/59/240_F_185915956_Pw79jOJmiCb9OcHTP927IoKrdO8UHM7W.jpg">
 <meta charset="utf-8">
 <meta name="mobile-web-app-capable" content="yes">
 <meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1.0, maximum-scale=1.0"/>
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<script>
-  var websock;
-  var t0 = 0;
-  var t1 = 0;
-  var state = 0;
-    
-  const socketMessageListener = (event) => {
-    t0 = performance.now();
-          JSONobj = JSON.parse(event.data);
-      state = JSONobj.controlOn;
-      document.getElementById('timeOn').innerHTML = 'Ligado há: ' + JSONobj.timeOn; 
-      if(JSONobj.controlOn == 'ON') {
-        document.getElementById('btn').style.background = '#E3E3E3';
-        document.getElementById('btn').style.color = '#434343';
-        document.getElementById('btn').style["boxShadow"]="0px 0px 0px 0px #E3E3E3";
-      }
-      else {
-        document.getElementById('btn').style.background = '#434343';
-        document.getElementById('btn').style.color = '#E3E3E3';
-        document.getElementById('btn').style["boxShadow"]="0px 0px 0px 8px #434343";
-      }
-  };
-
-  const socketOpenListener = (event) => {
-    console.log('Connected');
-  };
-
-  const socketCloseListener = (event) => {;
-    if (websock) {
-      console.error('Disconnected.');
-    }
-    websock = new WebSocket("ws://" + window.location.hostname + ":88/");
-    websock.addEventListener('open', socketOpenListener);
-    websock.addEventListener('message', socketMessageListener);
-    websock.addEventListener('close', socketCloseListener);
-  };
-
-  socketCloseListener();
-  
-  function button() {
-    if(state === 'OFF') {
-      websock.send('LEDonoff=ON');
-    }
-  }
-  function forceReload() {
-    setInterval(verify, 2000);
-  }
-  function verify() {
-    if(t1 - t0 > 5000) {
-      location.reload(0);
-    }
-    t1 = performance.now();
-  }
-</script>
 <style>
 body, html {
   height: 98%;
@@ -139,11 +86,65 @@ h5{
     </div>
   </div>
 <div id="mid">
-  <a href="#" id="btn" ONCLICK='button()'><i class="fa fa-bolt"></i></a>
+  <a href="#" id="btn"><i class="fa fa-bolt"></i></a>
 </div>
 <div id="footer">
   <h2 id="timeOn"></h2>
 </div>
 </body>
+<script>
+  var websock;
+  var t0 = 0;
+  var t1 = 0;
+  var state = 0;
+  var button = document.getElementById('btn');
+  const socketMessageListener = (event) => {
+    t0 = performance.now();
+          JSONobj = JSON.parse(event.data);
+      state = JSONobj.controlOn;
+      document.getElementById('timeOn').innerHTML = `Ligado há: ${JSONobj.timeOn}`; 
+      if(JSONobj.controlOn == 'ON') {
+        button.style.background = '#E3E3E3';
+        button.style.color = '#434343';
+        button.style.boxShadow = '0px 0px 0px 0px #E3E3E3';
+      }
+      else {
+        button.style.background = '#434343';
+        button.style.color = '#E3E3E3';
+        button.style.boxShadow = '0px 0px 0px 8px #434343';
+      }
+  };
+
+  const socketOpenListener = (event) => {
+    console.log('Connected');
+  };
+
+  const socketCloseListener = (event) => {;
+    if (websock) {
+      console.error('Disconnected.');
+    }
+    websock = new WebSocket("ws://" + window.location.hostname + ":88/");
+    websock.addEventListener('open', socketOpenListener);
+    websock.addEventListener('message', socketMessageListener);
+    websock.addEventListener('close', socketCloseListener);
+  };
+
+  socketCloseListener();
+  
+  button.onclick = function (event) {
+    if(state === 'OFF') {
+      websock.send('LEDonoff=ON');
+    }
+  }
+  function forceReload() {
+    setInterval(verify, 2000);
+  }
+  function verify() {
+    if(t1 - t0 > 5000) {
+      location.reload(0);
+    }
+    t1 = performance.now();
+  }
+</script>
 <html>
 )=====";

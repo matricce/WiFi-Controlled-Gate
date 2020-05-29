284a536ae0f40cc2fd0acf1157d43fe5f76a7f88
matricce <ruviansanches20@gmail.com>
2020-05-29 12:49:54 -0300

    Update script.js
    pingInterval estava sendo definido posteriormente seu uso, gerando instabilidade

--- data/script.js
+++ data/script.js
@@ -2,13 +2,13 @@ let url = "ws://" + window.location.hostname + ":1337/";
 let button;
 let timer;
 let btn_sound;
-let connected;
-let visible;
+let connected = 0;
+let visible = 1;
 let state;
 let buttonBg;
 let clock;
-let pingInterval;
-let untilResponse;
+let pingInterval = 250;
+let untilResponse = 2000;
 let timeout;
 
 function init() {
@@ -17,10 +17,6 @@ function init() {
   btn_sound = document.getElementById('tickSound');
   buttonBg = document.getElementsByClassName('buttonHolder')[0];
   clock = setInterval('doPing()', pingInterval);
-  connected = false;
-  visible = true;
-  pingInterval = 250;
-  untilResponse = 2000;
   wsConnect(url);
 }
 init();
@@ -36,17 +32,18 @@ function onOpen(evt) {
   setConnected();
 }
 function onClose(evt) {
-  setDisconnected();
-  setTimeout(function() { wsConnect(url) }, 2000);
+  setDisconnected('onClose');
+  setTimeout(function() { wsConnect(url) }, untilResponse);
 }
 function onMessage(evt) {
   // console.log("Received: " + evt.data);
+  console.log("Received");
   JSONobj = JSON.parse(evt.data);
   state = JSONobj.controlState;
   timer.innerHTML = `Ligado há: ${JSONobj.timeOn}`; 
   buttonBg.style.backgroundColor = 'rgba(255,0,0,0)';
   clearTimeout(timeout);
-  timeout = setTimeout('setDisconnected()', untilResponse);
+  timeout = setTimeout('setDisconnected(\'timeout\')', untilResponse);
     if(state == 'ON') {;
       btn_sound.play();
       button.style.animation = 'buttonClick .1s forwards';
@@ -81,6 +78,7 @@ function doPing() {
 function onPause() {
   visible = false;
   clearInterval(clock);
+  clearTimeout(timeout);//???
   buttonBg.style.backgroundColor = 'rgba(255,0,0,0.5)';
  }
 function onResume() {
@@ -99,8 +97,8 @@ function setConnected() {
   connected = true;
   buttonBg.style.backgroundColor = 'rgba(0,0,0,0)';
 }
-function setDisconnected() {
-  console.log("Disconnected");
+function setDisconnected(reason) {
+  console.log("Disconnected, Reason: " + reason);
   connected = false;
   buttonBg.style.backgroundColor = 'rgba(255,0,0,0.5)';
 }
